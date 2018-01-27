034dfc2463dff02d60bf4621336a0ae3ed65e365
Martoon_00 <Martoon_00@mail.ru>
2018-01-27 04:34:49 +0300

    Add tests for range tree

--- src/Nondominated.java
+++ src/Nondominated.java
@@ -1,20 +1,15 @@
 import com.sun.istack.internal.Nullable;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
-import java.util.TreeSet;
-import java.util.function.BinaryOperator;
-import java.util.function.Consumer;
-import java.util.function.Function;
-import java.util.function.Predicate;
+import java.util.*;
+import java.util.function.*;
+import java.util.stream.Collectors;
 
 /**
  * Created by Martoon.
  */
 public class Nondominated {
     public static void main(String[] args) {
-
+        TestSegmentTree.seriesTestSum(100000, 1000);
     }
 
     static <T> T findMedian(List<T> list) {
@@ -42,20 +37,18 @@ public class Nondominated {
         private Tree tree;
 
         public static <K extends Comparable<K>, V, M> SegmentTree<K, V, M>
-        mkSegmentTree(Function<V, K> getKey, Function<V, M> getSummary, BinaryOperator<M> addSummaries, List<V> values) {
+        make(Function<V, K> getKey, Function<V, M> getSummary, BinaryOperator<M> addSummaries, List<V> values) {
             List<K> keys = new ArrayList<>();
             for (V value : values) {
                 keys.add(getKey.apply(value));
             }
             keys.sort(null);
             List<K> uniqKeys = Util.removeAdjacentDuplicates(keys);
-            uniqKeys.add(keys.get(0));
-            for (int i = 1; i < keys.size(); i++) {
-                if (keys.get(i - 1) != keys.get(i)) {
-                    uniqKeys.add(keys.get(i));
-                }
-            }
-            return new SegmentTree<>(getKey, getSummary, addSummaries).buildTree(uniqKeys);
+            BinaryOperator<M> addSummariesSafe = (a, b) ->
+                    a == null ? b
+                            : b == null ? a
+                            : addSummaries.apply(a, b);
+            return new SegmentTree<>(getKey, getSummary, addSummariesSafe).buildTree(uniqKeys);
         }
 
         private SegmentTree<K, V, M> buildTree(List<K> sortedKeys) {
@@ -75,6 +68,18 @@ public class Nondominated {
                 List<K> rightKeys = keys.subList(m, keys.size());
                 return new Node(buildTreeImpl(leftKeys), buildTreeImpl(rightKeys), k -> k.compareTo(med) < 0, range);
             }
+        }
+
+        public M request(Range<K> range) {
+            return tree.request(range);
+        }
+
+        public void add(V value) {
+            tree.modify(getKey.apply(value), s -> s.add(value));
+        }
+
+        public void remove(V value) {
+            tree.modify(getKey.apply(value), s -> s.remove(value));
         }
 
         private SegmentTree(Function<V, K> getKey, Function<V, M> getSummary, BinaryOperator<M> addSummaries) {
@@ -99,6 +104,7 @@ public class Nondominated {
 
         private class Leaf extends Tree {
             private final Set<V> values = new TreeSet<>();
+
             public Leaf(Range<K> range) {
                 super(range);
             }
@@ -147,11 +153,13 @@ public class Nondominated {
             public M request(Range<K> reqRange) {
                 if (reqRange.equals(range)) {  // totally matches
                     return summary;
+                } else if (reqRange.left.compareTo(range.right) > 0 || range.left.compareTo(reqRange.right) > 0) {  // totally in nowhere
+                    return null;
                 } else if (reqRange.right.compareTo(right.range.left) < 0) {  // go left
                     return left.request(reqRange);
                 } else if (reqRange.left.compareTo(left.range.right) > 0) {  // go right
                     return right.request(reqRange);
-                } else {
+                } else {  // go in both directions
                     M leftRes = left.request(new Range<>(reqRange.left, this.range.right));
                     M rightRes = right.request(new Range<>(this.range.left, reqRange.right));
                     return addSummaries.apply(leftRes, rightRes);
@@ -184,7 +192,7 @@ public class Nondominated {
         }
 
         public boolean contains(V value) {
-            return left.compareTo(value) < 0 && value.compareTo(right) < 0;
+            return left.compareTo(value) <= 0 && value.compareTo(right) <= 0;
         }
 
         @Override
@@ -195,6 +203,11 @@ public class Nondominated {
             Range<?> range = (Range<?>) o;
 
             return left.equals(range.left) && right.equals(range.right);
+        }
+
+        @Override
+        public String toString() {
+            return String.format("(%s, %s)", left, right);
         }
     }
 
@@ -243,4 +256,110 @@ public class Nondominated {
             return unique;
         }
     }
+
+    static class Gen {
+        protected final Random random;
+
+        public Gen() {
+            this.random = new Random();
+        }
+
+        public Gen(int seed) {
+            this.random = new Random(seed);
+        }
+
+        protected Supplier<Integer> integer(int bound) {
+            return () -> random.nextInt(2 * bound + 1) - bound;
+        }
+
+        public static <V> Supplier<List<V>> vectorOf(int k, Supplier<V> gen) {
+            return () -> {
+                ArrayList<V> res = new ArrayList<>();
+                for (int i = 0; i < k; i++) {
+                    res.add(gen.get());
+                }
+                return res;
+            };
+        }
+
+        protected <V> Supplier<List<V>> sublistOf(Supplier<List<V>> gen) {
+            return () -> {
+                List<V> list = gen.get();
+                ArrayList<V> res = new ArrayList<>();
+                for (V v : list) {
+                    if (random.nextBoolean()) {
+                        res.add(v);
+                    }
+                }
+                return res;
+            };
+        }
+
+        protected <V extends Comparable<V>> Supplier<Range<V>> rangeOf(Supplier<V> gen) {
+            return () -> new Range<>(gen.get(), gen.get());
+        }
+    }
+
+    static class TestSegmentTree {
+        public static void testSum(int seed, int n) {
+            new Gen(seed) {{
+                int inputBound = Math.max(10, n * 3);
+                List<Integer> inputUnsafe = Gen.vectorOf(n, integer(inputBound)).get();
+                List<Integer> input = inputUnsafe.stream().distinct().collect(Collectors.toList());
+                List<Integer> chosen = sublistOf(() -> input).get();
+                List<Range<Integer>> requests = vectorOf(1, rangeOf(integer(inputBound + 1))).get();
+
+                SegmentTree<Integer, Integer, Integer> tree =
+                        SegmentTree.make(Function.identity(), Function.identity(), Integer::sum, input);
+                for (Integer v : chosen) {
+                    tree.add(v);
+                }
+                ArrayList<Integer> segTreeResults = new ArrayList<>();
+                for (Range<Integer> request : requests) {
+                    segTreeResults.add(tree.request(request));
+                }
+
+                ArrayList<Integer> sortedChosen = new ArrayList<>(chosen);
+                Collections.sort(sortedChosen);
+                ArrayList<Integer> dumbResults = new ArrayList<>();
+                for (Range<Integer> request : requests) {
+                    Integer result = null;
+                    for (Integer value : sortedChosen) {
+                        if (request.contains(value)) {
+                            result = result == null ? 0 : result;
+                            result += value;
+                        }
+                    }
+                    dumbResults.add(result);
+                }
+
+                assert dumbResults.size() == segTreeResults.size();
+                if (dumbResults.equals(segTreeResults)) {
+                    System.out.println("OK!");
+                } else {
+                    for (int i = 0; i < dumbResults.size(); i++) {
+                        Integer expected = dumbResults.get(i);
+                        Integer got = segTreeResults.get(i);
+                        if (!expected.equals(got)) {
+                            System.out.println("Test failure");
+                            System.out.println("For all points: " + input);
+                            System.out.println("For chosen: " + chosen);
+                            System.out.println("For request: " + requests.get(i));
+                            System.out.println("Expected " + expected + ", got " + got);
+                            throw new AssertionError("Test failed");
+                        }
+                    }
+                    throw new IllegalStateException("What failed eventually??");
+                }
+            }};
+        }
+
+        public static void seriesTestSum(int times, int n) {
+            for (int i = 1; i <= times; i++) {
+                System.out.println("Iteration #" + i);
+                TestSegmentTree.testSum(i, n);
+            }
+        }
+    }
+
 }
